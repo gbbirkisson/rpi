0fc9a20380c9f9a28704f021a6a984483279befd
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2019-01-10 17:17:22 +0100

    Test raspicam

--- cmd/rpi-server/cmd/root.go
+++ cmd/rpi-server/cmd/root.go
@@ -7,43 +7,75 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/gbbirkisson/rpi"
-	proto "github.com/gbbirkisson/rpi/proto"
+	"github.com/dhowden/raspicam"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	grpc "google.golang.org/grpc"
 )
 
 var cfgFile string
+
+// var rootCmd = &cobra.Command{
+// 	Use:   "rpi-server",
+// 	Short: "Raspberry PI IO server",
+// 	Long:  `A gRPC server that allows you to do IO operations on the Raspberry PI`,
+// 	RunE: func(cmd *cobra.Command, args []string) error {
+// 		log.Printf("rpi server started")
+// 		ip := viper.GetString("ip")
+// 		port := viper.GetString("port")
+// 		address := ip + ":" + port
+
+// 		srv := grpc.NewServer()
+
+// 		proto.RegisterCommonServer(srv, &rpi.CommonServerImpl{})
+
+// 		if cmd.Flag("gpio").Value.String() == "true" {
+// 			log.Printf("adding gpio service")
+// 			proto.RegisterGpioServer(srv, &rpi.GpioServerImpl{})
+// 		}
+
+// 		lis, err := net.Listen("tcp", address)
+// 		if err != nil {
+// 			log.Printf("failed starting server: %v\n", err)
+// 			os.Exit(1)
+// 		}
+
+// 		log.Printf("listening to %s\n", address)
+// 		log.Fatal(srv.Serve(lis))
+// 		return nil
+// 	},
+// }
 
 var rootCmd = &cobra.Command{
 	Use:   "rpi-server",
 	Short: "Raspberry PI IO server",
 	Long:  `A gRPC server that allows you to do IO operations on the Raspberry PI`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		log.Printf("rpi server started")
-		ip := viper.GetString("ip")
-		port := viper.GetString("port")
-		address := ip + ":" + port
-
-		srv := grpc.NewServer()
-
-		proto.RegisterCommonServer(srv, &rpi.CommonServerImpl{})
-
-		if cmd.Flag("gpio").Value.String() == "true" {
-			log.Printf("adding gpio service")
-			proto.RegisterGpioServer(srv, &rpi.GpioServerImpl{})
-		}
-
-		lis, err := net.Listen("tcp", address)
+	Run: func(cmd *cobra.Command, args []string) {
+		listener, err := net.Listen("tcp", "0.0.0.0:8000")
 		if err != nil {
-			log.Printf("failed starting server: %v\n", err)
-			os.Exit(1)
+			fmt.Fprintf(os.Stderr, "listen: %v", err)
 		}
+		log.Println("Listening on 0.0.0.0:8000")
 
-		log.Printf("listening to %s\n", address)
-		log.Fatal(srv.Serve(lis))
-		return nil
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "accept: %v", err)
+			}
+			log.Printf("Accepted connection from: %v\n", conn.RemoteAddr())
+			go func() {
+				s := raspicam.NewStill()
+				errCh := make(chan error)
+				go func() {
+					for x := range errCh {
+						fmt.Fprintf(os.Stderr, "%v\n", x)
+					}
+				}()
+				log.Println("Capturing image...")
+				raspicam.Capture(s, conn, errCh)
+				log.Println("Done")
+				conn.Close()
+			}()
+		}
 	},
 }
 
