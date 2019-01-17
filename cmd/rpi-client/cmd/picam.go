c4177a50ce6555ca86d3f60b7cbfe3d42a2fcbf3
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2019-01-17 22:18:35 +0100

    Update client

--- cmd/rpi-client/cmd/picam.go
+++ cmd/rpi-client/cmd/picam.go
@@ -4,14 +4,12 @@ import (
 	"bytes"
 	"image"
 	"image/jpeg"
-	"io"
 	"os"
 
 	"github.com/spf13/viper"
 
 	"github.com/gbbirkisson/rpi"
 	helper "github.com/gbbirkisson/rpi/cmd"
-	proto "github.com/gbbirkisson/rpi/pkg/proto"
 	"github.com/spf13/cobra"
 )
 
@@ -19,43 +17,22 @@ var picamCmd = &cobra.Command{
 	Use:   "picam",
 	Short: "Get frame from the PiCam",
 	Run: func(_ *cobra.Command, args []string) {
-		conn, err := rpi.GrpcClientConnectionInsecure(viper.GetString("host"), viper.GetString("port"))
-		helper.ExitOnError("could not create client", err)
-		client := proto.NewPiCamServiceClient(conn)
 		ctx, cancel := getContext()
 		defer cancel()
 
-		stream, err := client.GetFrames(ctx, &proto.Void{})
-		helper.ExitOnError("error response from server", err)
-		defer stream.CloseSend()
+		conn, err := rpi.GrpcClientConnectionInsecure(viper.GetString("host"), viper.GetString("port"))
+		helper.ExitOnError("could not create client", err)
 
-		shouldStream := viper.GetBool("stream")
-		for {
-			res, err := stream.Recv()
-			helper.ExitOnError("error getting frame", err)
-			r := bytes.NewReader(res.ImageBytes)
-			imageData, _, err := image.Decode(r)
-			helper.ExitOnError("unable to decode image", err)
+		cam := &rpi.PiCam{Connection: conn}
+		raw, err := cam.GetFrame(ctx)
+		helper.ExitOnError("could not get image", err)
 
-			opts := jpeg.Options{}
-			opts.Quality = 80
+		r := bytes.NewReader(raw)
+		imageData, _, err := image.Decode(r)
 
-			if len(args) > 0 && !shouldStream {
-				// Create file
-				f, err := os.Create(args[0])
-				helper.ExitOnError("could not create file", err)
-				defer f.Close()
-				io.Copy(f, bytes.NewReader(res.ImageBytes))
-				// err = jpeg.Encode(f, imageData, &opts)
-				// helper.ExitOnError("unable to encode image", err)
-			} else {
-				err = jpeg.Encode(os.Stdout, imageData, &opts)
-				helper.ExitOnError("unable to encode image", err)
-			}
-			if !shouldStream {
-				break
-			}
-		}
+		opts := jpeg.Options{}
+		opts.Quality = 80
+		jpeg.Encode(os.Stdout, imageData, &opts)
 	},
 }
 
